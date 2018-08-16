8d96e1ba2f895cfad2df04da598d2f69ccbc7b83
davidrubio2 <davidrubio2@gmail.com>
2018-08-16 16:20:00 -0600

    Update newquery.sql
    ProcesoDeAutorizacion  change to default value null and add test table

--- base de datos/newquery.sql
+++ base de datos/newquery.sql
@@ -9,5 +9,15 @@ Monto REAL NOT NULL,
 Edad INT NOT NULL,
 TarjetaDeCredito BOOLEAN NOT NULL,
 PlazoDeInteres REAL NOT NULL,
-ProcesoDeAutorizacion BOOLEAN NOT NULL
+ProcesoDeAutorizacion BOOLEAN DEFAULT NULL
 );
+
+INSERT INTO Usuario(Usuario) VALUES('david');
+INSERT INTO Solicitud(ID,Monto,Edad,TarjetaDeCredito,PlazoDeInteres)
+VALUES('1','100','18',TRUE,'4');
+drop table Usuario;
+drop table Solicitud;
+
+
+DELETE from Solicitud where id = '1'
+DELETE from Usuario where id = '1'
