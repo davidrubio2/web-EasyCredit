f173c1207d34006288d7e678dc35cd455f87c883
davidrubio2 <davidrubio2@gmail.com>
2018-08-16 15:54:02 -0600

    Update newquery.sql
    se agregaron las tablas

--- base de datos/newquery.sql
+++ base de datos/newquery.sql
@@ -1,8 +1,12 @@
-create database EasyCredit;
-
-use EasyCredit;
-
 create table Usuario(
+ID SERIAL PRIMARY KEY NOT NULL,
+Usuario VARCHAR(20) NOT NULL
+);
 
-
+create table Solicitud(
+ID SERIAL  NOT NULL,
+Monto REAL NOT NULL,
+Edad INT NOT NULL,
+TarjetaDeCredito BOOLEAN NOT NULL,
+PlazoDeInteres BOOLEAN NOT NULL
 );
