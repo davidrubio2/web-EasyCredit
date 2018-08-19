23e633f9d30fb9ede6d7448f6859485c8aad501a
david <davidrubio2@gmail.com>
2018-08-18 22:45:37 -0600

    chnage get to post

--- node/myExpressApp/routes/index.js
+++ node/myExpressApp/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../queries');
-router.get('/api/consultarGrabarUsuario', db.consultarGrabarUsuario);
+router.post('/api/consultarGrabarUsuario', db.consultarGrabarUsuario);
 
 module.exports = router;
 
