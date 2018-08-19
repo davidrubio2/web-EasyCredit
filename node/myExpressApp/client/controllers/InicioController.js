6cf61f756de6f4c0c8f7b62719c6468ca7f21ac7
david <davidrubio2@gmail.com>
2018-08-18 22:29:57 -0600

    add control validation of the page  and call the service

--- node/myExpressApp/client/controllers/InicioController.js
+++ node/myExpressApp/client/controllers/InicioController.js
@@ -1,1 +1,16 @@
+var InicioSession = angular.module("InicioSession", ['InicioSessionAppService']);
+InicioSession.controller("InicioSessionController", ['$scope', 'InicioSessionService', function ($scope, InicioSessionService) {
+        $scope.first_name = /^[a-zA-Z ]+$/;
+        $scope.InicioSessionAppService = []
+        $scope.Ingresar = function () {
+                var data = {
+                        "Username": $scope.firstname,                
+                }
+                InicioSessionService.IngresarService(data).then(function (response) {
+                         $scope.InicioSessionAppService.push(response.data)
+                         $scope.Session = response.data;   
+                });
+			
+        }
+}]);
 
