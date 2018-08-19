8955d94fc7fc17321257901f933306419a7dad22
david <davidrubio2@gmail.com>
2018-08-19 11:50:33 -0600

    add control

--- node/myExpressApp/client/controllers/PanelController.js
+++ node/myExpressApp/client/controllers/PanelController.js
@@ -1,1 +1,20 @@
+var PanelSession = angular.module("PanelSession", []);
+PanelSession.controller("PanelSesssionController", ['$scope',  function ($scope) {
+ 
 
+    function GetURLParameter(sParam)
+    {
+        var sPageURL = window.location.hash.substring(1); 
+        var sURLVariables = sPageURL.split('?');
+        for (var i = 0; i < sURLVariables.length; i++)
+        {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam)
+            {
+                return sParameterName[1];
+            }
+        }
+    }
+    $scope.Id = GetURLParameter('Id');
+
+}]);
