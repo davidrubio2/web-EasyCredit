08f01b5fc8f3138b1131e1ad95f6b681d6bf3ba6
davidrubio2 <davidrubio2@gmail.com>
2018-08-22 16:20:46 -0600

    se agrego formularioController.js para la pagina

---
+++ node/myExpressApp/client/controllers/FormularioController.js
@@ -0,0 +1,9 @@
+var FormularioSession = angular.module("FormularioSession", []);
+FormularioSession.controller("FormularioSessionController", ['$scope', function ($scope,) {
+
+        $scope.Solicitar = function () {
+               
+			var c = "asd";
+        }
+}]);
+
