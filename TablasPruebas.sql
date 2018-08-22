0771012ee8728db166204653d84c77679cc488a0
davidrubio2 <davidrubio2@gmail.com>
2018-08-22 09:09:47 -0600

    update TablasPruebas

---
+++ TablasPruebas.sql
@@ -0,0 +1,34 @@
+﻿CREATE DATABASE EasyCredit
+
+create table Usuario(
+IdUsuario SERIAL PRIMARY KEY NOT NULL,
+Nombre VARCHAR(20) NOT NULL,
+fec_Inserccion TIMESTAMP DEFAULT TIMENOW() NOT NULL
+);
+
+create table Solicitud(
+IdSolicitud SERIAL  NOT NULL,
+IdUsuario INT NOT NULL,
+Monto REAL NOT NULL,
+Edad INT NOT NULL,
+TarjetaDeCredito BOOLEAN NOT NULL,
+PlazoDeInteres REAL NOT NULL,
+ProcesoDeAutorizacion BOOLEAN DEFAULT NULL,
+fec_Creacion TIMESTAMP DEFAULT TIMENOW() NOT NULL,
+fec_Aceptacion TIMESTAMP 
+);
+
+INSERT INTO Usuario(Nombre) VALUES('david');
+
+INSERT INTO Solicitud(IdUsuario,
+Monto,Edad,TarjetaDeCredito,PlazoDeInteres,ProcesoDeAutorizacion)
+VALUES('1','100','20',TRUE,'7',NULL);
+
+
+drop table Usuario;
+
+drop table Solicitud;
+
+
+DELETE from Solicitud where id = '1'
+DELETE from Usuario where id = '1'
