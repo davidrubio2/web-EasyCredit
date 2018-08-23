74a5eb4d1374eaa9a9c6306575e8cc991bfe0acc
davidrubio2 <davidrubio2@gmail.com>
2018-08-22 23:38:55 -0600

    se actualizaron unas variables

--- node/myExpressApp/client/controllers/InicioController.js
+++ node/myExpressApp/client/controllers/InicioController.js
@@ -1,10 +1,10 @@
 var InicioSession = angular.module("InicioSession", ['InicioSessionAppService']);
 InicioSession.controller("InicioSessionController", ['$scope', 'InicioSessionService', function ($scope, InicioSessionService) {
-        $scope.first_name = /^[a-zA-Z ]+$/;
+        $scope.user_name = /^[a-zA-Z ]+$/;
         $scope.InicioSessionAppService = []
         $scope.Ingresar = function () {
                 var data = {
-                        "Username": $scope.firstname,                
+                        "Username": $scope.username,                
                 }
                 InicioSessionService.IngresarService(data).then(function (response) {
                          $scope.InicioSessionAppService.push(response.data)
