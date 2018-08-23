9f3a8f8e0842dc55966ec1355d7d784e522523c8
davidrubio2 <davidrubio2@gmail.com>
2018-08-22 23:40:34 -0600

    se agrego FormularioServices y se implemento nueva solicitud

--- node/myExpressApp/client/controllers/FormularioController.js
+++ node/myExpressApp/client/controllers/FormularioController.js
@@ -1,9 +1,47 @@
-var FormularioSession = angular.module("FormularioSession", []);
-FormularioSession.controller("FormularioSessionController", ['$scope', function ($scope,) {
+var FormularioSession = angular.module("FormularioSession", ['FormularioSessionAppService']);
+FormularioSession.controller("FormularioSessionController", ['$scope','FormularioSessionService', function ($scope,FormularioSessionService) {
+
+        $scope.FormularioSessionAppService = []
+     
+
+                function GetURLParameter(sParam)
+        {
+            var sPageURL = window.location.hash.substring(1); 
+            var sURLVariables = sPageURL.split('?');
+            for (var i = 0; i < sURLVariables.length; i++)
+            {
+                var sParameterName = sURLVariables[i].split('=');
+                if (sParameterName[0] == sParam)
+                {
+                    return sParameterName[1];
+                }
+            }
+        }
+        $scope.Id = GetURLParameter('Id');
+
 
         $scope.Solicitar = function () {
-               
-			var c = "asd";
+
+                if ($scope.tieneTarjeta == true) {
+                        var tieneTarjeta = "SI TIENE";
+                    }
+                    else{
+                        var tieneTarjeta = "NO TIENE";  
+                    }
+
+
+                var data = {
+                        "Id": $scope.Id,
+                        "Monto": $scope.monto,       
+                        "Edad": $scope.edad,       
+                        "TarjetaDeCredito": tieneTarjeta,                     
+                        "PlazoDeInteres": $scope.plazo,                     
+                }
+                
+                FormularioSessionService.PostNuevaSolicitud(data).then(function (response) {
+                        $scope.FormularioSessionAppService.push(response.data)
+                        $scope.NuevaSolicitud = response.data.data;
+                });
         }
 }]);
 
---
+++ node/myExpressApp/client/services/FormularioServices.js
@@ -0,0 +1,12 @@
+angular.module('FormularioSessionAppService', [])
+        .factory('FormularioSessionService', ['$http', function ($http) {
+                var handler = {
+                        PostNuevaSolicitud: function (data) {
+                                var url = 'api/nuevaSolicitud'
+                                return $http.post(url,data)
+                                
+                },
+        }
+
+        return handler;
+}]);	
