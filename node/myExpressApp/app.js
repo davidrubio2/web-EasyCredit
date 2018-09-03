d65cab27479f2aa8cae981b2f8c4b4cb7ea6de6b
david <davidrubio2@gmail.com>
2018-09-02 23:48:20 -0600

    Eliminando codigo innecesario

--- node/myExpressApp/app.js
+++ node/myExpressApp/app.js
@@ -5,13 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 var options = { root: __dirname };
 app.get('/' , function(req,res) {
   res.sendFile('./views/Index.html',options);
@@ -24,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use(express.static(__dirname + '/client')); 
 
 /*var server = app.listen(8080, "192.168.0.29", function() {
